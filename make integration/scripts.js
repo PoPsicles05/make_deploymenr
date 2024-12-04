9c94946682281ed863177c0ecc831179c2343842
PoPsicles05 <131449285+PoPsicles05@users.noreply.github.com>
2024-12-04 19:31:48 +0530

    Delete make integration directory

--- make integration/scripts.js
+++
@@ -1,7 +0,0 @@
-const role = sessionStorage.getItem('role'); // Assume this is set on login
-if (role === 'admin') {
-  document.getElementById('admin-link').style.display = 'block';
-}
-if (role !== 'patient') {
-  // Show Admin/Staff-specific elements
-}
